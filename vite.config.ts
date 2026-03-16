49c6c38f0e79f353f1c1269b954da3f63b2d1269
BrazierJhaMari <jebrazier@cps.edu>
2026-03-16 16:21:03 -0500

    service

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,39 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
-    ],
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
+      manifest: {
+        name: "Maneuver",
+        short_name: "Maneuver",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    host: true, // same as "--host" flag
+    host: true,
     allowedHosts: [
       '.ngrok-free.dev',
       '.ngrok-free.app',
